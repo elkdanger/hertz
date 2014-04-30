8d4679f6d2773141315a9de052e563bf73a3be32
Steve Hobbs <elkdanger@googlemail.com>
2014-04-30 17:35:37 +0100

    Added Grunt file

---
+++ src/Gruntfile.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),		
+	});
+	
+	grunt.registerTask("default", "Log some stuff..", function() {
+		grunt.log.write("Starting..");
+	});
+}
