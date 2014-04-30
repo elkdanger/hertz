1ea7896814893324010976e4c3e0e72722c54f84
Steve Hobbs <elkdanger@googlemail.com>
2014-04-30 15:05:38 +0100

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,14 @@
+﻿var http = require('http');
+var express = require('express');
+var connect = require('connect');
+var serveStatic = require('serve-static');
+
+var port = process.env.port || 1337;
+
+var app = express();
+
+app.use(serveStatic('public', { 'index': ['index.html'] }));
+
+var server = app.listen(port, function () {
+    console.log("Server started: http://localhost:" + port);
+});
