da52b5460c72488aa6084e560f949b7da6305499
Steve Hobbs <elkdanger@googlemail.com>
2014-05-02 17:32:51 +0100

    Gruntfile changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,19 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON("package.json"),
-
-		clean: ["publish"]
+		src_files: "src/*",
+		output_dir: "publish",
+		clean: ["<%= output_dir %>"],
+		copy: {
+			main: {
+				src: "<%= src_files %>",
+				dest: "<%= output_dir %>"
+			}
+		}
 	});
 
 	grunt.loadNpmTasks("grunt-contrib-clean");
+	grunt.loadNpmTasks("grunt-contrib-copy");
 
-	grunt.registerTask("default", ["clean"]);
+	grunt.registerTask("default", ["copy"]);
 }
