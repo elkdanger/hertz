6d3daee46c94994abecc64caf06d8720f1af8a0e
Steve Hobbs <elkdanger@googlemail.com>
2014-05-06 23:19:02 +0100

    New deployment file

--- deploy.cmd
+++ deploy.cmd
@@ -88,38 +88,34 @@ goto :EOF
 :Deployment
 echo Handling node.js deployment.
 
-:: 1. KuduSync
-IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-  IF !ERRORLEVEL! NEQ 0 goto error
-)
-
-:: 2. Select node version
+:: Select node version
 call :SelectNodeVersion
 
-:: 3. Install npm packages
-IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
-  pushd "%DEPLOYMENT_TARGET%"
+:: Install npm packages
+IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
+  pushd "%DEPLOYMENT_SOURCE%"
   call :ExecuteCmd !NPM_CMD! install --production
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
 
-:: 4. Install grunt-cli
-IF EXIST "%DEPLOYMENT_TARGET%\Gruntfile.js" (
-	pushd "%DEPLOYMENT_TARGET%"
+:: Install grunt-cli
+IF EXIST "%DEPLOYMENT_SOURCE%\Gruntfile.js" (
+	pushd "%DEPLOYMENT_SOURCE%"
 	call :ExecuteCmd !NPM_CMD! install grunt-cli
 	IF !ERRORLEVEL! NEQ 0 goto error
 	
 	:: 5. Run grunt	
-	@echo %DEPLOYMENT_TARGET%
-	call :ExecuteCmd .\node_modules\.bin\grunt.cmd
+	call :ExecuteCmd ".\node_modules\.bin\grunt.cmd"
 	
 	popd
 )
 
-
-
+:: KuduSync
+IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%\publish" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  IF !ERRORLEVEL! NEQ 0 goto error
+)
 
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
