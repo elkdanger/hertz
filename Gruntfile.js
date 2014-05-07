e9eafbf812e4a2ac5ac2da43e8a6ac92907378f6
Steve Hobbs <elkdanger@googlemail.com>
2014-05-07 14:16:12 +0100

    Changed grunt message.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,30 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-	grunt.initConfig({
-		pkg: grunt.file.readJSON("package.json"),
-		src_files: "**",
-		output_dir: "publish/",
-		clean: ["<%= output_dir %>"],
-		copy: {
-			main: {
-				src: ["<%= src_files %>", "!*njsproj", "!*.sln", "!*.suo", "../node_modules"],
-				dest: "<%= output_dir %>",
-				expand: true,
-				cwd: "src"
-			},
-			modules: {
-			    src: "node_modules/**",
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        src_files: "**",
+        output_dir: "publish/",
+        clean: ["<%= output_dir %>"],
+        copy: {
+            main: {
+                src: ["<%= src_files %>", "!*njsproj", "!*.sln", "!*.suo", "../node_modules"],
+                dest: "<%= output_dir %>",
+                expand: true,
+                cwd: "src"
+            },
+            modules: {
+                src: "node_modules/**",
                 dest: "<%= output_dir %>"
-			}
-		}
-	});
+            }
+        }
+    });
 
-	/*grunt.loadNpmTasks("grunt-contrib-clean");
-	grunt.loadNpmTasks("grunt-contrib-copy");
+    /*grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-copy");
 
-	grunt.registerTask("default", ["clean", "copy"]);*/
+    grunt.registerTask("default", ["clean", "copy"]);*/
 
-	grunt.registerTask("default", function() {
-        console.log("Running Grunt..");
-	});
+    grunt.registerTask("default", function () {
+        console.log("No tasks here yet. Ignoring..");
+    });
 }
